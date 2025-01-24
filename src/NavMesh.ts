dd997d4b4ebafab3f3222e90fdc090af80762810
Felix Moses <felix@tacocat.com>
2025-01-24 11:11:40 +0100

    Funnel pt. 1

--- src/NavMesh.ts
+++ src/NavMesh.ts
@@ -1,4 +1,4 @@
-import { midpoint, Poly, Vec, Pather, contains } from "./utils"
+import { midpoint, Poly, Vec, Pather, contains, intersects, loop, orientation } from "./utils"
 
 export class NavMesh extends Pather<Vec> {
     paths = new Map<Vec, Vec[]>()
@@ -22,6 +22,8 @@ export class NavMesh extends Pather<Vec> {
     }
 
     getCell(coord: Vec) {
+        if (this.cells.has(coord)) return coord
+
         for (const [cell, poly] of this.cells.entries()) {
             if (contains(poly, coord)) {
                 return cell
@@ -37,11 +39,11 @@ export class NavMesh extends Pather<Vec> {
 
         const path = super.path(ca, cb)
 
-        return [
+        return this.funnel([
             a,
-            ...path,
+            ...path.slice(1, -1),
             b
-        ]
+        ])
     }
 
     neighbors(vec: Vec): Vec[] {
@@ -50,5 +52,46 @@ export class NavMesh extends Pather<Vec> {
 
     distance(a: Vec, b: Vec): number {
         return a.distance(b)
+    }
+
+    /**
+     * Pulls the path tight using the funnel algorithm.
+     * 
+     * source: https://digestingduck.blogspot.com/2010/03/simple-stupid-funnel-algorithm.html
+     */
+    funnel(p: Vec[]) {
+        // STEP 1: get edges
+        const left: Vec[] = [p[0]]
+        const right: Vec[] = [p[0]]
+        for (let i = 0; i < p.length - 1; i++) {
+            const cell = this.cells.get(this.getCell(p[i]))
+            const segment = [p[i], p[i + 1]] as [Vec, Vec]
+            loop(cell, (a, b) => {
+                if (intersects([a, b], segment)) {
+                    if (orientation(...segment, a) === 1) {
+                        left.push(a)
+                        right.push(b)
+                    } else {
+                        left.push(b)
+                        right.push(a)
+                    }
+                }
+            })
+        }
+        left.push(p[p.length - 1])
+        right.push(p[p.length - 1])
+
+        // STEP 2: 
+
+        let leftindex = 0
+
+        const path = [p[0]]
+
+        
+
+        path.push(p[p.length - 1])
+
+        // Step 3: Profit
+        return [...left, ...right.reverse()]
     }
 }
